3dd92509461706a9b6a773383e6562efd44e910b
lolorol <lalesle@microsoft.com>
2020-01-30 01:38:51 +0000

    fixing rg length

--- module.tf
+++ module.tf
@@ -1,6 +1,7 @@
 module "caf_name_vm" {
-  source  = "aztfmod/caf-naming/azurerm"
-  version = "~> 0.1.0"
+  # source  = "aztfmod/caf-naming/azurerm"
+  # version = "~> 0.1.0"
+  source = "git://github.com/aztfmod/terraform-azurerm-caf-naming.git?ref=ll-fixes"
   
   name    = var.name
   type    = "gen"
